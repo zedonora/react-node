5eb8c7821ce3002f953187dfb7975063acd46322
humanplusgit <positivekyg@human-plus.co.kr>
2020-05-20 18:07:28 +0900

    changed

--- ch1/front/pages/index.js
+++ ch1/front/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Link from "next/link";
 const Home = () => {
-	return (
-		<>
-			<Link href='/about'>
-				<a>about</a>
-			</Link>
-			<div>Hello, Next!</div>
-		</>
-	);
+  return (
+    <>
+      <Link href="/about">
+        <a>about</a>
+      </Link>
+      <div>Hello, Next!</div>
+    </>
+  );
 };
 
 export default Home;

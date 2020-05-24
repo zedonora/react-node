74bf0ab15cf29c4c3ebcf8e6396633c3535ab4ef
zedonora <timeundtime@gmail.com>
2020-05-24 10:23:06 +0900

    form completed

--- ch1/front/pages/signup.js
+++ ch1/front/pages/signup.js
@@ -4,96 +4,117 @@ import AppLayout from "../components/AppLayout";
 import { Form, Input, Checkbox, Button } from "antd";
 
 const Signup = () => {
-	const [id, setId] = useState("");
-	const [nick, setNick] = useState("");
-	const [password, setPassword] = useState("");
-	const [passwordCheck, setPasswordCheck] = useState("");
-	const [term, setTerm] = useState(false);
+  const [id, setId] = useState("");
+  const [nick, setNick] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [term, setTerm] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
 
-	const onSubmit = () => {};
+  const onSubmit = () => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
 
-	const onChangeId = (e) => {
-		setId(e.target.value);
-	};
+    if (!term) {
+      return setTermError(true);
+    }
+    console.log({
+      id,
+      nick,
+      password,
+      passwordCheck,
+      term
+    });
+  };
 
-	const onChangeNick = (e) => {
-		setNick(e.target.value);
-	};
+  const onChangeId = e => {
+    setId(e.target.value);
+  };
 
-	const onChangePassword = (e) => {
-		setPassword(e.target.value);
-	};
+  const onChangeNick = e => {
+    setNick(e.target.value);
+  };
 
-	const onChangePasswordCheck = (e) => {
-		setPasswordCheck(e.target.value);
-	};
+  const onChangePassword = e => {
+    setPassword(e.target.value);
+  };
 
-	const onChangeTerm = (e) => {
-		setTerm(e.target.value);
-	};
+  const onChangePasswordCheck = e => {
+    setPasswordError(e.target.value !== password);
+    setPasswordCheck(e.target.value);
+  };
 
-	return (
-		<>
-			<Head>
-				<title>NodeBird</title>
-				<link
-					rel='stylesheet'
-					href='https://cdnjs.cloudflare.com/ajax/libs/antd/4.2.4/antd.css'
-				/>
-			</Head>
-			<AppLayout>
-				<Form onSubmit={onSubmit} style={{ padding: 10 }}>
-					<div>
-						<label htmlFor='user-id'>아이디</label>
-						<br />
-						<Input nmae='user-id' value={id} required onChange={onChangeId} />
-					</div>
-					<div>
-						<label htmlFor='user-nick'>닉네임</label>
-						<br />
-						<Input
-							nmae='user-nick'
-							value={nick}
-							required
-							onChange={onChangeNick}
-						/>
-					</div>
-					<div>
-						<label htmlFor='user-password'>비밀번호</label>
-						<br />
-						<Input
-							nmae='user-password'
-							type='password'
-							value={password}
-							required
-							onChange={onChangePassword}
-						/>
-					</div>
-					<div>
-						<label htmlFor='user-password-check'>비밀번호 체크</label>
-						<br />
-						<Input
-							nmae='user-password-check'
-							type='password'
-							value={passwordCheck}
-							required
-							onChange={onChangePasswordCheck}
-						/>
-					</div>
-					<div>
-						<Checkbox name='user-term' value={term} onChange={onChangeTerm}>
-							kyg 말을 잘 들을 것을 동의합니다.
-						</Checkbox>
-					</div>
-					<div>
-						<Button type='primary' htmlType='submit'>
-							가입하기
-						</Button>
-					</div>
-				</Form>
-			</AppLayout>
-		</>
-	);
+  const onChangeTerm = e => {
+    setTermError(false);
+    setTerm(e.target.checked);
+  };
+
+  return (
+    <>
+      <Head>
+        <title> NodeBird </title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.2.4/antd.css"
+        />
+      </Head>
+      <AppLayout>
+        <Form onFinish={onSubmit} style={{ padding: 10 }}>
+          <div>
+            <label htmlFor="user-id"> 아이디 </label> <br />
+            <Input nmae="user-id" value={id} required onChange={onChangeId} />
+          </div>
+          <div>
+            <label htmlFor="user-nick"> 닉네임 </label> <br />
+            <Input
+              nmae="user-nick"
+              value={nick}
+              required
+              onChange={onChangeNick}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password"> 비밀번호 </label> <br />
+            <Input
+              nmae="user-password"
+              type="password"
+              value={password}
+              required
+              onChange={onChangePassword}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password-check"> 비밀번호 체크 </label> <br />
+            <Input
+              nmae="user-password-check"
+              type="password"
+              value={passwordCheck}
+              required
+              onChange={onChangePasswordCheck}
+            />
+            {passwordError && (
+              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+            )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              kyg 말을 잘 들을 것을 동의합니다.
+            </Checkbox>
+            {termError && (
+              <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
+            )}
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <Button type="primary" htmlType="submit">
+              가입하기
+            </Button>
+          </div>
+        </Form>
+      </AppLayout>
+    </>
+  );
 };
 
 export default Signup;

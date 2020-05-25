2bbd3a0b1f6f548a5c35405892aad83c496c838d
zedonora <timeundtime@gmail.com>
2020-05-26 05:16:34 +0900

    proptype add

--- ch1/front/components/AppLayout.js
+++ ch1/front/components/AppLayout.js
@@ -1,32 +1,37 @@
 import React from "react";
 import Link from "next/link";
+import PropTypes from "prop-types";
 import { Menu, Input, Button } from "antd";
 
 const AppLayout = ({ children }) => {
-	return (
-		<div>
-			<Menu mode='horizontal'>
-				<Menu.Item key='home'>
-					<Link href='/'>
-						<a>노드버드</a>
-					</Link>
-				</Menu.Item>
-				<Menu.Item key='profile'>
-					<Link href='/profile'>
-						<a>프로필</a>
-					</Link>
-				</Menu.Item>
-				<Menu.Item key='email'>
-					<Input.Search enterButton style={{ verticalAlign: "middle" }} />
-				</Menu.Item>
-			</Menu>
-			<Link href='/signup'>
-				<a>
-					<Button>회원가입</Button>
-				</a>
-			</Link>
-			{children}
-		</div>
-	);
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="profile">
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="email">
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+      </Menu>
+      <Link href="/signup">
+        <a>
+          <Button>회원가입</Button>
+        </a>
+      </Link>
+      {children}
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node
 };
 export default AppLayout;
--- ch1/front/pages/_app.js
+++ ch1/front/pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import PropTypes from "prop-types";
 import AppLayout from "../components/AppLayout";
 const NodeBird = ({ Component }) => {
   return (
@@ -16,6 +17,10 @@ const NodeBird = ({ Component }) => {
       </AppLayout>
     </>
   );
+};
+
+NodeBird.propTypes = {
+  Component: PropTypes.elementType
 };
 
 export default NodeBird;

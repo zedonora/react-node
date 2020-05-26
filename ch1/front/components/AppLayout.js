b4a03dfc80c55fe4121240acc9571d4bf14c1af3
zedonora <timeundtime@gmail.com>
2020-05-27 04:14:51 +0900

    antd grid add

--- ch1/front/components/AppLayout.js
+++ ch1/front/components/AppLayout.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu, Input, Button } from "antd";
+import { Menu, Input, Button, Row, Col, Card, Avatar } from "antd";
 
+const dummy = {
+  nickname: "영관김",
+  Post: [],
+  Followings: [],
+  Followers: [],
+};
 const AppLayout = ({ children }) => {
   return (
     <div>
@@ -21,17 +27,50 @@ const AppLayout = ({ children }) => {
           <Input.Search enterButton style={{ verticalAlign: "middle" }} />
         </Menu.Item>
       </Menu>
-      <Link href="/signup">
-        <a>
-          <Button>회원가입</Button>
-        </a>
-      </Link>
-      {children}
+      <Row>
+        <Col xs={24} md={6}>
+          <Card
+            actions={[
+              <div key="twit">
+                짹짹
+                <br />
+                {dummy.Post.length}
+              </div>,
+              <div key="following">
+                팔로잉
+                <br />
+                {dummy.Followings.length}
+              </div>,
+              <div key="follower">
+                팔로워
+                <br />
+                {dummy.Followers.length}
+              </div>,
+            ]}
+          >
+            <Card.Meta
+              avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+              title={dummy.nickname}
+            />
+          </Card>
+          <Link href="/signup">
+            <a>
+              <Button>회원가입</Button>
+            </a>
+          </Link>
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          세번째
+        </Col>
+      </Row>
     </div>
   );
 };
 
 AppLayout.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
 export default AppLayout;

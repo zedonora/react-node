24b6603aa9466498cc77fa36d6274b8e41919f6f
zedonora <timeundtime@gmail.com>
2020-05-30 06:06:22 +0900

    profile

--- ch1/front/pages/profile.js
+++ ch1/front/pages/profile.js
@@ -1,9 +1,52 @@
 import React from "react";
+import { Form, Input, Button, List, Card } from "antd";
+import { Icon } from "@ant-design/compatible";
 const Profile = () => {
   return (
-    <>
-      <div>내 프로필</div>
-    </>
+    <div>
+      <Form
+        style={{
+          marginBottom: "20px",
+          border: "1px dolid #9d9d9",
+          padding: "20px",
+        }}
+      >
+        <Input addonBefore="닉네임" />
+        <Button type="primary"> 수정 </Button>
+      </Form>
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div> 팔로잉 목록 </div>}
+        loadMore={<Button style={{ width: "100%" }}> 더 보기 </Button>}
+        bordered
+        dataSource={["제로초", "바보", "노드버드오피셜"]}
+        renderItem={(item) => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<Icon key="stop" type="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div> 팔로워 목록 </div>}
+        loadMore={<Button style={{ width: "100%" }}> 더 보기 </Button>}
+        bordered
+        dataSource={["제로초", "바보", "노드버드오피셜"]}
+        renderItem={(item) => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<Icon key="stop" type="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
   );
 };
 

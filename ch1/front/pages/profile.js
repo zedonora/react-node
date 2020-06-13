831892426e4ace03d9e4393d725d5dd8f4e02a7f
zedonora <timeundtime@gmail.com>
2020-06-14 08:00:45 +0900

    generator

--- ch1/front/pages/profile.js
+++ ch1/front/pages/profile.js
@@ -3,43 +3,80 @@ import { Button, List, Card } from "antd";
 import { StopOutlined } from "@ant-design/icons";
 import NicknameEditForm from "../components/NicknameEditForm";
 const Profile = () => {
-  return (
-    <div>
-      <NicknameEditForm />
-      <List
-        style={{ marginBottom: "20px" }}
-        grid={{ gutter: 4, xs: 2, md: 3 }}
-        size="small"
-        header={<div> 팔로잉 목록 </div>}
-        loadMore={<Button style={{ width: "100%" }}> 더 보기 </Button>}
-        bordered
-        dataSource={["제로초", "바보", "노드버드오피셜"]}
-        renderItem={(item) => (
-          <List.Item style={{ marginTop: "20px" }}>
-            <Card actions={[<StopOutlined />]}>
-              <Card.Meta description={item} />
-            </Card>
-          </List.Item>
-        )}
-      />
-      <List
-        style={{ marginBottom: "20px" }}
-        grid={{ gutter: 4, xs: 2, md: 3 }}
-        size="small"
-        header={<div> 팔로워 목록 </div>}
-        loadMore={<Button style={{ width: "100%" }}> 더 보기 </Button>}
-        bordered
-        dataSource={["제로초", "바보", "노드버드오피셜"]}
-        renderItem={(item) => (
-          <List.Item style={{ marginTop: "20px" }}>
-            <Card actions={[<Icon key="stop" type="stop" />]}>
-              <Card.Meta description={item} />
-            </Card>
-          </List.Item>
-        )}
-      />
-    </div>
-  );
-};
+        return ( <
+                div >
+                <
+                NicknameEditForm / >
+                <
+                List style = {
+                    { marginBottom: "20px" }
+                }
+                grid = {
+                    { gutter: 4, xs: 2, md: 3 }
+                }
+                size = "small"
+                header = { < div > 팔로잉 목록 < /div>}
+                    loadMore = { < Button style = {
+                            { width: "100%" }
+                        } > 더 보기 < /Button>}
+                        bordered
+                        dataSource = {
+                            ["제로초", "바보", "노드버드오피셜"]
+                        }
+                        renderItem = {
+                            (item) => ( <
+                                List.Item style = {
+                                    { marginTop: "20px" }
+                                } >
+                                <
+                                Card actions = {
+                                    [ < StopOutlined / > ]
+                                } >
+                                <
+                                Card.Meta description = { item }
+                                /> < /
+                                Card > <
+                                /List.Item>
+                            )
+                        }
+                        /> <
+                        List
+                        style = {
+                            { marginBottom: "20px" }
+                        }
+                        grid = {
+                            { gutter: 4, xs: 2, md: 3 }
+                        }
+                        size = "small"
+                        header = { < div > 팔로워 목록 < /div>}
+                            loadMore = { < Button style = {
+                                    { width: "100%" }
+                                } > 더 보기 < /Button>}
+                                bordered
+                                dataSource = {
+                                    ["제로초", "바보", "노드버드오피셜"]
+                                }
+                                renderItem = {
+                                    (item) => ( <
+                                        List.Item style = {
+                                            { marginTop: "20px" }
+                                        } >
+                                        <
+                                        Card actions = {
+                                            [ < Icon key = "stop"
+                                                type = "stop" / >
+                                            ]
+                                        } >
+                                        <
+                                        Card.Meta description = { item }
+                                        /> < /
+                                        Card > <
+                                        /List.Item>
+                                    )
+                                }
+                                /> < /
+                                div >
+                            );
+                        };
 
-export default Profile;
+                        export default Profile;

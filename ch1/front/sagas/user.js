d7ed8b3a7bfed0be05d231829cded509ca305064
dongwhek@usc.edu <dongwhek@usc.edu>
2020-08-15 08:05:45 +0900

    new

--- ch1/front/sagas/user.js
+++ ch1/front/sagas/user.js
@@ -1,55 +1,55 @@
 import {
-  all,
-  fork,
-  call,
-  put,
-  takeLatest,
-  take,
-  delay,
-  takeEvery,
+	all,
+	fork,
+	call,
+	put,
+	takeLatest,
+	take,
+	delay,
+	takeEvery,
 } from "redux-saga/effects";
 import {
-  LOG_IN,
-  LOG_IN_SUCCESS,
-  LOG_IN_FAILURE,
-  SIGN_UP_REQUEST,
+	LOG_IN,
+	LOG_IN_SUCCESS,
+	LOG_IN_FAILURE,
+	SIGN_UP_REQUEST,
 } from "../reducers/user";
 
 const HELLO_SAGA = "HELLO SAGA";
 
 function loginAPI() {
-  // 서버에 요청을 보내는 부분
+	// 서버에 요청을 보내는 부분
 }
 function* login() {
-  try {
-    yield call(loginAPI);
-    yield put({
-      // put은 dispatch 동일
-      type: LOG_IN_SUCCESS,
-    });
-  } catch (e) {
-    // 에러 부분13
+	try {
+		yield call(loginAPI);
+		yield put({
+			// put은 dispatch 동일
+			type: LOG_IN_SUCCESS,
+		});
+	} catch (e) {
+		// 에러 부분14
 
-    console.error(e);
-    yield put({
-      type: LOG_IN_FAILURE,
-    });
-  }
+		console.error(e);
+		yield put({
+			type: LOG_IN_FAILURE,
+		});
+	}
 }
 
 function* watchLogin() {
-  yield takeEvery(LOG_IN, login);
+	yield takeEvery(LOG_IN, login);
 }
 
 function* hello() {
-  yield delay(1000);
-  yield put({
-    type: "BYE_SAGA",
-  });
+	yield delay(1000);
+	yield put({
+		type: "BYE_SAGA",
+	});
 }
 
 function* watchHello() {
-  yield takeLatest(HELLO_SAGA, hello);
+	yield takeLatest(HELLO_SAGA, hello);
 }
 
 // function *watchHello() {
@@ -63,19 +63,19 @@ function* watchHello() {
 // }
 
 function* watchLogin() {
-  while (true) {
-    yield take(LOG_IN);
-    yield delay(2000);
-    yield put({
-      type: LOG_IN_SUCCESS,
-    });
-  }
+	while (true) {
+		yield take(LOG_IN);
+		yield delay(2000);
+		yield put({
+			type: LOG_IN_SUCCESS,
+		});
+	}
 }
 
 function* watchSignUp() {
-  yield takeEvery(SIGN_UP_REQUEST, signUp);
+	yield takeEvery(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
-  yield all([fork(watchLogin), fork(watchSignUp)]);
+	yield all([fork(watchLogin), fork(watchSignUp)]);
 }

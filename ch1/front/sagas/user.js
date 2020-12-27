c1f4915761f92aaeac4b3ba1ec43bae28272c1da
dongwhek@usc.edu <dongwhek@usc.edu>
2020-12-27 16:21:53 +0900

    type

--- ch1/front/sagas/user.js
+++ ch1/front/sagas/user.js
@@ -1,34 +1,40 @@
-import {
-	all,
-	fork,
-	call,
-	put,
-	takeLatest,
-	take,
-	delay,
-	takeEvery,
-} from "redux-saga/effects";
-import {
-	LOG_IN,
-	LOG_IN_SUCCESS,
-	LOG_IN_FAILURE,
-	SIGN_UP_REQUEST,
-} from "../reducers/user";
+import
+	{
+		all,
+		fork,
+		call,
+		put,
+		takeLatest,
+		take,
+		delay,
+		takeEvery,
+	} from "redux-saga/effects";
+import
+	{
+		LOG_IN,
+		LOG_IN_SUCCESS,
+		LOG_IN_FAILURE,
+		SIGN_UP_REQUEST,
+	} from "../reducers/user";
 
 const HELLO_SAGA = "HELLO SAGA";
 
-function loginAPI() {
+function loginAPI ()
+{
 	// 서버에 요청을 보내는 부분
 }
-function* login() {
-	try {
+function* login ()
+{
+	try
+	{
 		yield call(loginAPI);
 		yield put({
 			// put은 dispatch 동일
 			type: LOG_IN_SUCCESS,
 		});
-	} catch (e) {
-		// 에러 부분5
+	} catch (e)
+	{
+		// 에러 부분2
 
 		console.error(e);
 		yield put({
@@ -37,18 +43,21 @@ function* login() {
 	}
 }
 
-function* watchLogin() {
+function* watchLogin ()
+{
 	yield takeEvery(LOG_IN, login);
 }
 
-function* hello() {
+function* hello ()
+{
 	yield delay(1000);
 	yield put({
 		type: "BYE_SAGA",
 	});
 }
 
-function* watchHello() {
+function* watchHello ()
+{
 	yield takeLatest(HELLO_SAGA, hello);
 }
 
@@ -62,8 +71,10 @@ function* watchHello() {
 //   }
 // }
 
-function* watchLogin() {
-	while (true) {
+function* watchLogin ()
+{
+	while (true)
+	{
 		yield take(LOG_IN);
 		yield delay(2000);
 		yield put({
@@ -72,10 +83,12 @@ function* watchLogin() {
 	}
 }
 
-function* watchSignUp() {
+function* watchSignUp ()
+{
 	yield takeEvery(SIGN_UP_REQUEST, signUp);
 }
 
-export default function* userSaga() {
+export default function* userSaga ()
+{
 	yield all([fork(watchLogin), fork(watchSignUp)]);
 }
